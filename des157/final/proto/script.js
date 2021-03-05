40f288d11adc3c262d281851b52c75818dd24fac
Jose Murillo <jodmurillo@ucdavis.edu>
2021-03-05 00:47:42 -0800

    Prototype 1 Update: added mp3, added font

--- des157/final/proto/script.js
+++ des157/final/proto/script.js
@@ -2,4 +2,10 @@
 
     'use strict';
     
+    const albumCover = document.getElementById('album4');
+    const song = new Audio('media/idorucut.mp3');
+
+    albumCover.addEventListener('click', function(){
+        song.play();
+    });
 }());

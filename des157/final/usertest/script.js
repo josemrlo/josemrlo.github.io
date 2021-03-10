2187fe5f2a936fe44324b9de8059402e8cb9402f
Jose Murillo <jodmurillo@ucdavis.edu>
2021-03-09 17:06:50 -0800

    user test added

--- des157/final/usertest/script.js
+++ des157/final/usertest/script.js
@@ -1,6 +1,8 @@
 (function(){
 
     'use strict';
+
+    alert("Hello! Please 1. View in fully extended window. 2. Click on the album cover. 3. Click on Jose Murillo");
     
     const albumCover = document.getElementById('album4');
     const song = new Audio('media/idorucut.mp3');

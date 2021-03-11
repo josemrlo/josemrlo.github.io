6aae63c87b293c1506e150e11b7de77f83356821
Jose Murillo <jodmurillo@ucdavis.edu>
2021-03-11 11:17:58 -0800

    user test results added

--- des157/final/proto/script.js
+++ des157/final/proto/script.js
@@ -3,10 +3,10 @@
     'use strict';
     
     const albumCover = document.getElementById('album4');
-    const song = new Audio('media/idorucut.mp3');
+    const song = new Audio('media/beabodycut.mp3');
+    let playing = false;
 
     albumCover.addEventListener('click', function(){
         song.play();
-
     });
 }());

32cd3e216c36708acd30c71f84a418b6674ee9b7
Jose Murillo <jodmurillo@ucdavis.edu>
2021-03-12 20:48:17 -0800

    updated final

--- des157/final/finalproject/script.js
+++ des157/final/finalproject/script.js
@@ -3,16 +3,23 @@
     'use strict';
     
     const albumCover = document.getElementById('albumcover');
-    const playBtn = document.getElementById('play');
-    const pauseBtn = document.getElementById('pause');
-    const song = new Audio('media/realiticut.mp3');
+    const playPauseBtn = document.getElementById('playpause');
+    const song = document.getElementById('song');
+    let count = 0;
+    
 
-    playBtn.addEventListener('click', function(){
-        song.play();
-        pauseBtn.classList.remove("hidden");
-    });
-
-    pauseBtn.addEventListener('click', function(){
-        song.pause();
+    playPauseBtn.addEventListener('click', function(){
+        if(count == 0){
+            count = 1;
+            song.play();
+            playPauseBtn.src = "images/pause.svg";
+            albumCover.classList.add("playing");
+        }
+        else {
+            count = 0;
+            song.pause();
+            playPauseBtn.src = "images/play.svg";
+            albumCover.classList.remove("playing");
+        }
     });
 }());
